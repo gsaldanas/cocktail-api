f02b142dbb517f12864ac135f21a7e0d75f97617
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2023-01-09 15:04:13 +0100

    async await axios on search(console log)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 import "mvp.css";
 import "../css/style.scss";
-console.log("test");
-
-document.querySelector("form").onsubmit = (e) => {
+import axios from "axios";
+document.querySelector("form").onsubmit = async (e) => {
   e.preventDefault();
+  const value = document.querySelector("form input[type=text]").value;
+  console.log(value);
+  if (value.length >= 2) {
+    const response = await axios(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${value}`
+    );
+    console.log(response.data);
+  }
 };
